e51525ada4b01e1ec6c4ba5df399cd8309929b5d
Hyunuk Shin <shu914@naver.com>
2024-10-26 22:40:33 +0900

    Initial Commit

---
+++ MyHealthKitApp/MyHealthKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHealthKitAppApp.swift
+//  MyHealthKitApp
+//
+//  Created by 신현욱 on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MyHealthKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyHealthKitAppTests/MyHealthKitAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyHealthKitAppTests.swift
+//  MyHealthKitAppTests
+//
+//  Created by 신현욱 on 10/26/24.
+//
+
+import Testing
+@testable import MyHealthKitApp
+
+struct MyHealthKitAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
